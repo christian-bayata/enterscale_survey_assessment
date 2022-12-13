8e2c07b7d2c6b5c2f05751868497c3d018377855
christian-bayata <franksagie1@gmail.com>
2022-12-13 02:21:50 +0100

    Rabbit mq connection and service setup

---
+++ services/rabbitmq/service.js
@@ -0,0 +1,24 @@
+const connectionString = require("./connection");
+const open = require("amqplib").connect(connectionString);
+
+class RabbitMqService {
+  /**
+   *
+   * @param queue
+   * @param data
+   */
+  static publish(queue, data) {
+    open
+      .then(function (conn) {
+        return conn.createChannel();
+      })
+      .then(function (ch) {
+        return ch.assertQueue(queue).then(function (ok) {
+          return ch.sendToQueue(queue, Buffer.from(JSON.stringify(data)));
+        });
+      })
+      .catch(console.warn);
+  }
+}
+
+module.exports = RabbitMqService;
