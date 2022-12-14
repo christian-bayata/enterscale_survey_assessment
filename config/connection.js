2ff93a65de9bd99cf8582ad81bc37784896c8108
christian-bayata <franksagie1@gmail.com>
2022-12-14 13:38:29 +0100

    Production datbase config

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,12 @@ const environment = process.env.NODE_ENV || "development";
 let connectionString;
 
 switch (environment) {
-  case "production":
-    // Point the database credentials to the production DB here
-    connectionString = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-    break;
   case "test":
     connectionString = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.TEST_DB_NAME}`;
     break;
   case "production":
     connectionString = `${process.env.DATABASE_URL}`;
+    break;
   default:
     connectionString = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 }
